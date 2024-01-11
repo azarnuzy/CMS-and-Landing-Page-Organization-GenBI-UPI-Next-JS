079e5f2c8a89aec97f2fce7b888a911c1c14cb88
azarnuzy <azarnuzy@gmail.com>
2024-01-11 21:51:36 +0700

    feat: update site url in config.ts

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   title: 'GenBI UPI',
   description:
     'GenBI (Generasi Baru Indonesia) UPI merupakan komunitas penerima beasiswa Bank Indonesia di Universitas Pendidikan Indonesia',
-  url: 'https://genbiupi.vercel.app',
+  url: 'https://genbi-upi.vercel.app',
   keywords: [
     'genbi',
     'genbi upi',
