dd60c20f240fb8a6276d4c95ec59b13ba76a5df7
azarnuzy <azarnuzy@gmail.com>
2024-01-11 15:05:46 +0700

    feat: update header section layout and content

--- src/modules/landing-page/header-section.tsx
+++ src/modules/landing-page/header-section.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image';
 import React from 'react';
 
+import BaseLayout from '@/components/layouts/base';
+import NextImage from '@/components/NextImage';
+
 const HeaderSection = () => {
   return (
     <div
-      className='h-[50vh] md:h-[55vh] lg:h-[60vh] w-full relative'
+      className='h-[60vh] md:h-[65vh] lg:h-[70vh] w-full relative'
       style={{
         background:
           'linear-gradient(101deg, #041C3F 3.31%, #11418B 39.48%, #11418B 98.43%)',
@@ -16,10 +19,10 @@ const HeaderSection = () => {
         alt='ellipse bg'
         width={0}
         height={0}
-        sizes='40vw'
+        sizes='40vw z-0'
       />
       <Image
-        className='absolute w-full h-[60vh] md:h-[65vh] lg:h-[70vh] -left-[38vw] '
+        className='absolute w-full h-[60vh] md:h-[65vh] lg:h-[70vh] -left-[38vw] z-0'
         src='/images/line-pattern-head.svg'
         alt='ellipse bg'
         width={0}
@@ -29,6 +32,43 @@ const HeaderSection = () => {
           transform: 'rotate(-9deg)',
         }}
       />
+      <div className='h-full relative z-[2]'>
+        <BaseLayout>
+          <div className='h-[60vh] md:h-[65vh] lg:h-[70vh] grid items-center grid-cols-3'>
+            <div className='col-span-3 md:col-span-2 flex flex-col gap-3'>
+              <h1 className='text-neutral-100 h0'>
+                Membentuk <i>Generasi Baru</i>
+              </h1>
+              <div className='flex gap-4 items-center'>
+                <div className='flex flex-col gap-2.5 items-center bg-neutral-100 rounded-full p-[14px]'>
+                  <NextImage
+                    alt='Logo BI'
+                    src='/images/logo-bi-color.png'
+                    width={36}
+                    height={36}
+                  />
+                  <NextImage
+                    alt='Logo GenBI'
+                    src='/images/logo-genbi-color.png'
+                    width={38}
+                    height={38}
+                  />
+                </div>
+                <div className='flex flex-col gap-3'>
+                  <h1 className='text-neutral-100 h0'>Dengan Mencipatakan </h1>
+                  <h2 className='text-neutral-100'>Energi Untuk Negeri</h2>
+                </div>
+              </div>
+              <p className='text-lg font-medium text-neutral-100 w-[490px]'>
+                Kami <strong>GenBI UPI</strong> hadir untuk mewujudkan perubahan
+                nyata. Temukan kisah, prestasi, dan komitmen kami di sini.
+              </p>
+            </div>
+
+            <div className='col-span-3 md:col-span-1'></div>
+          </div>
+        </BaseLayout>
+      </div>
     </div>
   );
 };
