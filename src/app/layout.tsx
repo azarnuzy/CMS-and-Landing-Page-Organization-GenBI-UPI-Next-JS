590f733ff3c4ea446cdb9c6c6365cb4a8520ad9a
azarnuzy <azarnuzy@gmail.com>
2024-01-11 21:56:52 +0700

    feat: update og metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.title,
-    images: [`${siteConfig.url}/images/og.jpg`],
+    images: [`${siteConfig.url}/images/og.png`],
     type: 'website',
     locale: 'en_US',
   },
