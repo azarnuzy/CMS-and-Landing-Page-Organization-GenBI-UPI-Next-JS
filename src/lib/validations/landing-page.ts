8502b0ae049c7bf25b92fca50f7527b4d26c5a05
azarnuzy <azarnuzy@gmail.com>
2024-01-13 20:53:54 +0700

    feat: update validation schema for suggestion form

--- src/lib/validations/landing-page.ts
+++ src/lib/validations/landing-page.ts
@@ -1,13 +1,19 @@
 import { z } from 'zod';
 
 export const ValidationSchemaSuggestionForm = z.object({
-  name: z.string({
-    required_error: 'Nama harus diisi',
-  }),
-  email: z.string({
-    required_error: 'Email harus diisi',
-  }),
-  question: z.string({
-    required_error: 'Pertanyaan harus diisi',
-  }),
+  name: z
+    .string({
+      required_error: 'Nama harus diisi',
+    })
+    .min(1, 'Nama harus diisi'),
+  email: z
+    .string({
+      required_error: 'Email harus diisi',
+    })
+    .min(1, 'Email harus diisi'),
+  question: z
+    .string({
+      required_error: 'Pertanyaan harus diisi',
+    })
+    .min(1, 'Pertanyaan harus diisi'),
 });
