3bc39903a6f636617bc3b8238d8aad1397e29241
azarnuzy <azarnuzy@gmail.com>
2024-02-17 13:25:52 +0700

    feat: fix error type in about gnebi recoil

--- src/recoils/about-genbi/atom.ts
+++
@@ -1,8 +0,0 @@
-// import { atom } from 'recoil';
-
-// import { TDataManagement } from '@/types/managements';
-
-// export const activeManagementState = atom<TDataManagement | null>({
-//   key: 'activeManagementState',
-//   default: null,
-// });
