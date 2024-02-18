be9b6f6418a63e62950089f06b525e8c3b9688cb
azarnuzy <azarnuzy@gmail.com>
2024-02-18 15:52:43 +0700

    feat: implement sorting and pagination in gallery

--- src/modules/gallery/gallery-section.tsx
+++ src/modules/gallery/gallery-section.tsx
@@ -2,16 +2,17 @@
 
 import { DialogDescription, DialogTitle } from '@radix-ui/react-dialog';
 import Image from 'next/image';
-import { useSearchParams } from 'next/navigation';
-import React, { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useRef } from 'react';
+import { useRecoilState } from 'recoil';
 
 import 'react-image-lightbox/style.css';
-import './index.css';
 
 import { formatDate } from '@/lib/utils/general-function';
 import { useGetGalleries } from '@/hooks/galleries/hook';
 
 import BaseLayout from '@/components/layouts/base';
+import Pagination from '@/components/pagination';
 import {
   Dialog,
   DialogContent,
@@ -19,85 +20,100 @@ import {
   DialogTrigger,
 } from '@/components/ui/dialog';
 
-import { TGalleriesData } from '@/types/galleries';
+import { galleriesDataState } from '@/recoils/galleries/atom';
 
 const GallerySection = () => {
   const searchParams = useSearchParams();
+  const router = useRouter();
 
-  const [galleriesData, setGalleriesData] = useState<Array<TGalleriesData>>([
-    {
-      alt: '',
-      id: 0,
-      caption: '',
-      category: '',
-      created_at: '',
-      file_url: '',
-      mimetype: '',
-      post_id: 0,
-      updated_at: '',
-    },
-  ]);
+  const parentRef = useRef<HTMLDivElement>(null);
 
-  const { data } = useGetGalleries({
+  const [galleriesData, setGalleriesData] = useRecoilState(galleriesDataState);
+
+  const { data, refetch } = useGetGalleries({
     limit: 9,
     page: Number(searchParams.get('page')) || 1,
     sort: 'created_at',
-    type: searchParams.get('type') || 'desc',
+    type: searchParams.get('sort') || 'desc',
   });
 
-  useEffect(() => {
-    if (data) {
-      setGalleriesData(data.data);
+  const handlePageChange = async (page: number) => {
+    let sort = '';
+
+    if (searchParams.get('sort')) {
+      sort = `&sort=${searchParams.get('sort')}`;
     }
-  }, [data]);
+
+    if (parentRef.current) {
+      parentRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+
+    router.replace(`/gallery?page=${page}${sort}`, { scroll: false });
+    await refetch();
+  };
+
+  useEffect(() => {
+    if (data && searchParams.get('page')) {
+      setGalleriesData(data?.data);
+    }
+  }, [data, galleriesData, searchParams, setGalleriesData]);
 
   return (
     <div className='-mt-[50px] sm:-mt-[75px] lg:-mt-[200px]  pb-10 relative z-10'>
       <BaseLayout>
-        <div className='grid grid-cols-3 gap-4 mx-auto'>
-          {galleriesData?.map((gallery, i) => (
-            <div
-              key={i}
-              className='min-h-[400px]  md:min-h-[250px] w-full col-span-3 md:col-span-1'
-            >
-              <div className='flex justify-center w-full h-full'>
-                <Dialog>
-                  <DialogTrigger className='group relative overflow-hidden'>
-                    <Image
-                      width={0}
-                      height={0}
-                      src={gallery.file_url || '/images/no-photo-available.png'}
-                      alt={gallery.alt || 'no photo available'}
-                      sizes='70vw'
-                      className='object-cover rounded-lg w-full h-full'
-                    />
-                    <div className='absolute transform translate-y-60 duration-300 ease-in-out transition-all group-hover:translate-y-0 bottom-0 bg-[#0C0505C7]  text-neutral-100 w-full p-4 text-start'>
-                      <p className='text-neutral-300'>
-                        {formatDate(gallery?.created_at)}
-                      </p>
-                      <h4>{gallery?.caption}</h4>
-                    </div>
-                  </DialogTrigger>
-                  <DialogContent className='transform scale-105 md:scale-150 '>
-                    <DialogHeader>
-                      <DialogTitle>{gallery.caption}</DialogTitle>
-                      <DialogDescription>
-                        <Image
-                          width={0}
-                          height={0}
-                          src={gallery.file_url}
-                          alt={gallery.alt}
-                          sizes='70vw'
-                          className='object-fill rounded-lg w-full h-full'
-                        />
-                      </DialogDescription>
-                    </DialogHeader>
-                  </DialogContent>
-                </Dialog>
+        <>
+          <div ref={parentRef} className='grid grid-cols-3 gap-4 mx-auto'>
+            {galleriesData?.map((gallery, i) => (
+              <div
+                key={i}
+                className='min-h-[400px]  md:min-h-[250px] w-full col-span-3 md:col-span-1'
+              >
+                <div className='flex justify-center w-full h-full'>
+                  <Dialog>
+                    <DialogTrigger className='group relative overflow-hidden'>
+                      <Image
+                        width={0}
+                        height={0}
+                        src={
+                          gallery.file_url || '/images/no-photo-available.png'
+                        }
+                        alt={gallery.alt || 'no photo available'}
+                        sizes='70vw'
+                        className='object-cover rounded-lg w-full h-full'
+                      />
+                      <div className='absolute transform translate-y-60 duration-300 ease-in-out transition-all group-hover:translate-y-0 bottom-0 bg-[#0C0505C7]  text-neutral-100 w-full p-4 text-start'>
+                        <p className='text-neutral-300'>
+                          {formatDate(gallery?.created_at)}
+                        </p>
+                        <h4>{gallery?.caption}</h4>
+                      </div>
+                    </DialogTrigger>
+                    <DialogContent className='transform scale-105 md:scale-150 '>
+                      <DialogHeader>
+                        <DialogTitle>{gallery.caption}</DialogTitle>
+                        <DialogDescription>
+                          <Image
+                            width={0}
+                            height={0}
+                            src={gallery.file_url}
+                            alt={gallery.alt}
+                            sizes='70vw'
+                            className='object-fill rounded-lg w-full h-full'
+                          />
+                        </DialogDescription>
+                      </DialogHeader>
+                    </DialogContent>
+                  </Dialog>
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+          <Pagination
+            currentPage={Number(data?.pagination?.currentPage) || 1}
+            totalPages={Number(data?.pagination?.totalPages) || 1}
+            onPageChange={handlePageChange}
+          />
+        </>
       </BaseLayout>
     </div>
   );
--- src/modules/gallery/header-section.tsx
+++ src/modules/gallery/header-section.tsx
@@ -1,7 +1,44 @@
+'use client';
+
 import Image from 'next/image';
-import React from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+
+import { useGetGalleries } from '@/hooks/galleries/hook';
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/select';
+
+import { galleriesDataState } from '@/recoils/galleries/atom';
 
 const HeaderGallerySection = () => {
+  const searchParams = useSearchParams();
+  const [, setGalleriesData] = useRecoilState(galleriesDataState);
+  const router = useRouter();
+  const { data, refetch } = useGetGalleries({
+    limit: 9,
+    page: Number(searchParams.get('page')) || 1,
+    sort: 'created_at',
+    type: searchParams.get('sort') || 'desc',
+  });
+
+  const handleChange = (e: string) => {
+    router.replace(`/gallery?sort=${e}`, { scroll: false });
+    refetch();
+  };
+
+  useEffect(() => {
+    if (data) {
+      setGalleriesData(data.data);
+    }
+  }, [data, setGalleriesData]);
+
   return (
     <div
       className='pt-20 lg:pt-0 min-h-[60vh] md:min-h-[65vh] lg:min-h-[70vh] w-full relative overflow-hidden'
@@ -38,6 +75,25 @@ const HeaderGallerySection = () => {
           Setiap gambar di sini adalah cerminan dari nilai-nilai yang kami
           junjung tinggi.
         </p>
+        <div className='flex justify-center'>
+          <Select
+            onValueChange={(e) => {
+              handleChange(e);
+            }}
+          >
+            <SelectTrigger className='w-fit bg-transparent text-neutral-100 border-neutral-100 focus:outline-none focus:ring-0 rounded-full'>
+              <SelectValue placeholder='Newest' />
+            </SelectTrigger>
+            <SelectContent className='text-start flex justify-start'>
+              <SelectItem className='text-start' value='desc'>
+                Newest
+              </SelectItem>
+              <SelectItem className='text-start' value='asc'>
+                Oldest
+              </SelectItem>
+            </SelectContent>
+          </Select>
+        </div>
       </div>
     </div>
   );
