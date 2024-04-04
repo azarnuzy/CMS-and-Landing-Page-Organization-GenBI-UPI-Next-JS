60c768a2927c38741afa3d28b8986903fe790eab
azarnuzy <azarnuzy@gmail.com>
2024-04-04 09:33:31 +0700

    fix: height access denied page

--- src/app/admin/denied/page.tsx
+++ src/app/admin/denied/page.tsx
@@ -10,7 +10,7 @@ const DeniedAdminPage = () => {
   return (
     <main>
       <section className='bg-white'>
-        <div className='layout flex min-h-screen flex-col items-center justify-center text-center text-black'>
+        <div className='layout flex min-h-[calc(100vh-71px)] flex-col items-center justify-center text-center text-black'>
           <RiAlarmWarningFill
             size={60}
             className='drop-shadow-glow animate-flicker text-red-500'
